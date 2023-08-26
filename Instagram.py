1acc5331bb178c84904afc860a839b6cab87c9f2
SACRED GAMES 3.06 <106374961+YounisXyz@users.noreply.github.com>
2023-08-26 16:16:13 +0500

    Create Instagram.py

---
+++ Instagram.py
@@ -0,0 +1,21 @@
+from XYZINSTAGRAM import Instaxyz
+ 
+import os
+try:
+     import bs4
+except (ModuleNotFoundError,ImportError):
+     os.system('pip install bs4')
+try:
+     import requests
+except (ModuleNotFoundError,ImportError):
+     os.system('pip install requests')
+ 
+def main():
+    os.system('git pull')
+    os.system('clear')
+    Instaxyz.false()
+ 
+if __name__ == '__main__':
+   main()
+ 
+ 
