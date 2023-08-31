1094da6e12cd0660153ece5e5dee8d4a730a17f5
SACRED GAMES 3.06 <106374961+YounisXyz@users.noreply.github.com>
2023-08-31 20:04:31 +0500

    Update run.py

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import os
 os.system('clear')
 x = 'Insta Script has been Stopped!'
 y = 'Instagram Cloning Has been Added In Official XYZ Tool'
-yz = 'Thanks for using Younis John tools'
+z = 'Thanks for using Younis John tools'
 print(x)
 print(y)
 print(z)
