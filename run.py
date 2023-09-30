f1433d742c8be9bda38571a94ae61b9307e5d975
SACRED GAMES 3.06 <106374961+YounisXyz@users.noreply.github.com>
2023-09-30 14:22:56 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 import os
-print(x)
-print(y)
 os.system('clear')
 x = '\033[0;97mScript has been updated 💬\n\n\033[1;32mInsta Cloning Has Been Added In \033[1;33mXYZ\033[0;97M Official Tool, Now You Can Run XYZ Tool All Features Applied!\n\t\033[1;36mThanks For Using Younis John Tools 👍❤️'
 y = '\033[0;97m--------------------------------------------------'
+print(x)
+print(y)
 input = '\n[ Press Enter To Go To \033[1;32mXYZ\033[0;97m Official ] '
 os.system('cd')
 os.system('clear')
