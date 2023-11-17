ffb5f894849761fd628151ac96fe32963f9acc1d
SACRED GAMES 3.06 <106374961+YounisXyz@users.noreply.github.com>
2023-11-17 08:15:23 +0500

    Delete run.py

--- run.py
+++
@@ -1,8 +0,0 @@
-import os
-os.system('clear')
-x = 'Script has been permanent deleted'
-y = 'The owner YounisXyz No more updates'
-z = '\033[1;32mThanks for using Younis john tools'
-print(x)
-print(y)
-print(z)
